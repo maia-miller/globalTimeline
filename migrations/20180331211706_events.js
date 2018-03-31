2d23f431305b98ed60b41ab2496aa17ba4500c18
Maia <maiamiller25@gmail.com>
2018-03-31 21:18:48 +1300

    Events table created

---
+++ migrations/20180331211706_events.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('events', table => {
+    table.increments('event_id')
+    table.string('name')
+    table.integer('year')
+    table.string('description')
+    table.string('link')
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('events')
+};
